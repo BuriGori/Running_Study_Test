d16cab5c77ca79b924107e9a36e75f81e3198a73
burigori <jamasa97@icloud.com>
2022-05-08 18:47:51 +0900

    init

---
+++ src/main/java/com/example/running_study_test/RunningStudyTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.running_study_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningStudyTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunningStudyTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/running_study_test/RunningStudyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.running_study_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningStudyTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
